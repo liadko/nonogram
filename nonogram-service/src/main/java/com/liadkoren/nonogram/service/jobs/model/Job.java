574e2e0b055ee17010fee7a10b3843505c402082
Liad Koren <liadkoren@gmail.com>
2025-10-02 21:27:18 +0300

    pom versions bullshit

--- nonogram-service/src/main/java/com/liadkoren/nonogram/service/jobs/model/Job.java
+++ nonogram-service/src/main/java/com/liadkoren/nonogram/service/jobs/model/Job.java
@@ -1,4 +1,17 @@
 package com.liadkoren.nonogram.service.jobs.model;
 
-public record Job() {
+@Entity
+@Table(name = "jobs")
+public class Job {
+	@Id
+	private String id;
+
+	@Enumerated(EnumType.STRING)
+	private JobStatus status;
+
+	@Lob
+	private String payloadJson;  // store Puzzle or URL serialized
+
+	private Instant createdAt;
+	private long budgetMs;
 }
